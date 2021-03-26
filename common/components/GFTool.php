7d103f657e9c861ec6aea7740141b08afb6d2308
wangweijie <wangweijie@163.com>
2021-03-26 21:05:07 +0800

    角色信息页面增加重置设备ID,

--- common/components/GFTool.php
+++ common/components/GFTool.php
@@ -231,5 +231,15 @@ class GFTool
         imagedestroy($image);
     }
 
+    public static function  getRandom( $param ){
+        $str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+        $key  =  "" ;
+        for ( $i =0; $i < $param ; $i ++)
+        {
+          $key  .=  $str {mt_rand(0,32)};     //生成php随机数
+        }
+        return  $key ;
+    }   
+
 
 }
