0457c7c79cafdd5ad9113aadd20f67939c7e5783
wangweijie <wangweijie@163.com>
2021-05-21 17:43:38 +0800

    添加console测试

--- console/controllers/DemoController.php
+++ console/controllers/DemoController.php
@@ -11,6 +11,11 @@ class DemoController extends Controller
     public function actionTest()
     {
         echo 'Test controller hello world'.date("r").'\n';
+        //总注册人数
+        $statisticsSql = "INSERT INTO lami_backend.test VALUES ();";
+        $statisticsData = Yii::$app->db->createCommand($statisticsSql)
+            ->execute();
+
         GFTool::writeLog('test controller '.date("r"));
     }
 }
