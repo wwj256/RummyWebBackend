e7e9258fc71149394761e2683d50024b08510cff
wangweijie <wangweijie@163.com>
2021-07-13 17:05:19 +0800

    修改币商英文

--- merchantBackend/views/layouts/header.php
+++ merchantBackend/views/layouts/header.php
@@ -43,7 +43,7 @@ use yii\helpers\Html;
 </div>
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">控制台</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">Console</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
