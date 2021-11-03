b9d298727e8aa2c392b16c4f733f3e34e1e5af6d
wangweijie <wangweijie@163.com>
2021-11-03 20:42:22 +0800

    修改币商，扣币至少扣100

--- merchantBackend/controllers/DealController.php
+++ merchantBackend/controllers/DealController.php
@@ -32,7 +32,7 @@ class DealController extends \yii\web\Controller
         if( $type == 0 ){
             $serverResponStr = HttpTool::doGet(Yii::$app->params['APIUrl']."houtai/checksms?ph=$phone&cd=$code");
             $serverRespon = json_decode($serverResponStr);
-            return Yii::$app->params['APIUrl']."houtai/checksms?ph=$phone&cd=$code"."___".$serverResponStr;
+            // return Yii::$app->params['APIUrl']."houtai/checksms?ph=$phone&cd=$code"."___".$serverResponStr;
             if( $serverRespon->code != 0 ){
                 return 'SMS code error!';
             }
